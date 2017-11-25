915168135101de7097e8810168d445b1b9b25bdf
Joshua Lovins <12jdlovins@gmail.com>
2017-11-24 21:00:50 -0500

    added database from enviroment

--- resurfedtools/settings.py
+++ resurfedtools/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -85,6 +86,10 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+db_from_env = dj_database_url.config()
+print(db_from_env)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -213,3 +218,5 @@ LIVE_MAP_URLS = {
     'https://files.resurfed.com/maplists/elite.txt',
     'https://files.resurfed.com/maplists/bhop.txt'
 }
+
+
